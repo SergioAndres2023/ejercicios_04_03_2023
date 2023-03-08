8a5ce167452db6e3abce379aff46d3a104b9a6cf
SergioAndres202 <sergiofourmi@gmail.com>
2023-03-08 20:19:39 +0100

    Ejercicios ultimo día

---
+++ frutas.js
@@ -0,0 +1,68 @@
+
+
+
+// console.log(frutas[2]);
+// console.log(frutas[5]);
+
+
+
+const frutas = [
+  {
+    name: 'Naranjas',
+    size: 'mediano',
+    color: 'naranja'
+  },
+  {
+    name: 'Lima',
+    size: 'pequeño',
+    color: 'verde'
+  },
+  {
+    name: 'Pera',
+    size: 'pequeño',
+    color: 'verde'
+  },
+  {
+    name: 'Melón',
+    size: 'grande',
+    color: 'verde'
+  },
+  {
+    name: 'Sandía',
+    size: 'grande',
+    color: 'verde'
+  },
+  {
+    name: 'Manzana',
+    size: 'pequeño',
+    color: 'verde'
+  }
+];
+
+if (frutas = 'pequeño' + 'verde') {
+  console.log("Fruta pequeña y verde");
+}
+//  else if (numero1 <= 0) {
+//   console.log("numero1 es negativo o distinto a cero");
+// }
+
+
+// console.log(frutas['pequeño' + 'verde']);
+
+// console.log(frutas);
+
+
+// let note = 10;
+// if (note <= 3) {
+//   console.log('Muy deficiente');
+// } else if (note >= 3 && note < 5) {
+//   console.log('Insuficiente');
+// }  else if (note >= 5 && note < 6) {
+//   console.log('Suficiente');
+// }  else if (note >= 6 && note < 7) {
+//   console.log('Bien');
+// }  else if (note >= 7 && note < 9) {
+//   console.log('Notable');
+// }  else if (note >= 9 && note <= 10) {
+//   console.log('Sobresaliente');
+// }
